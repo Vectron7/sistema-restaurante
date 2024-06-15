5e957a749eacbc41d35ebc76e67ef5fb3f96dbf8
Victoriaester741 <85125270+Victoriaester741@users.noreply.github.com>
2024-06-15 13:39:27 -0300

    Update Reserva.java

--- src/main/java/com/artur/controle/Reserva.java
+++ src/main/java/com/artur/controle/Reserva.java
@@ -1,40 +1,45 @@
 package com.artur.controle;
 
-import com.artur.interfaces.Identificacao;
-import com.artur.pessoas.Garcom;
+import com.artur.interfaces.Identificacao; // Importação da interface Identificacao
+import com.artur.pessoas.Garcom; // Importação da classe Garcom
 
+// Declaração da classe Reserva que implementa a interface Identificacao
 public class Reserva implements Identificacao {
 
-    private int idReserva;
-    private int numMesa;
-    private final int idGarcom;
-    private final int idCliente;
-    private final String nomeGarcom;
-    private String telefoneCliente;
-    private String nomeCliente;
-    private String dataReserva;
-    private String horaReserva;
+     // Declaração dos atributos da classe
+    private int idReserva;// Identificador único da reserva
+    private int numMesa; // Número da mesa reservada
+    private final int idGarcom; // Identificador do garçom associado à reserva
+    private final int idCliente; // Identificador do cliente que fez a reserva
+    private final String nomeGarcom; // Nome do garçom associado à reserva
+    private String telefoneCliente; // Telefone do cliente
+    private String nomeCliente; // Nome do cliente
+    private String dataReserva; // Data da reserva
+    private String horaReserva; // Hora da reserva
 
 
 
+    // Construtor da classe
     public Reserva(String dataReserva, String horaReserva, String nomeCliente, String telefoneCliente, int numMesa, Garcom g, int idCliente) {
-        this.dataReserva = dataReserva;
-        this.horaReserva = horaReserva;
-        this.telefoneCliente = telefoneCliente;
-        this.nomeCliente = nomeCliente;
-        this.numMesa = numMesa;
-        this.idGarcom = g.getId();
-        this.nomeGarcom = g.getNome();
-        this.idCliente = idCliente;
+        this.dataReserva = dataReserva; // Define a data da reserva
+        this.horaReserva = horaReserva; // Define a hora da reserva
+        this.telefoneCliente = telefoneCliente; // Define o telefone do cliente
+        this.nomeCliente = nomeCliente; // Define o nome do cliente
+        this.numMesa = numMesa; // Define o número da mesa 
+        this.idGarcom = g.getId(); // Obtém o ID do garçom 
+        this.nomeGarcom = g.getNome(); // Obtém o nome do garçom
+        this.idCliente = idCliente; // Define o ID do cliente
     }
 
 
 
+    // Implementação do método da interface Identificacao para obter o ID da reserva
     @Override
     public int getId() {
         return idReserva;
     }
 
+    // Métodos getters para obter os atributos da classe
     public String getTelefoneCliente() {
         return telefoneCliente;
     }
@@ -67,6 +72,8 @@ public class Reserva implements Identificacao {
         return idCliente;
     }
 
+    
+    // Métodos setters para modificar os atributos da classe
     public void setTelefoneCliente(String telefoneCliente) {
         this.telefoneCliente = telefoneCliente;
     }
