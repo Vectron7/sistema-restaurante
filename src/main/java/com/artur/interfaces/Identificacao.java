338d0115e3676898bfe615f3895cbb5d345861f5
Vectron7 <90154484+Vectron7@users.noreply.github.com>
2024-06-16 15:07:54 -0300

    Identificacao.java

--- src/main/java/com/artur/interfaces/Identificacao.java
+++ src/main/java/com/artur/interfaces/Identificacao.java
@@ -1,3 +1,5 @@
+//Esse código define uma interface chamada Identificacao que contém um método getId(), destinado a retornar um identificador único (ID) como um inteiro.
+
 package com.artur.interfaces;
 
 public interface Identificacao {
