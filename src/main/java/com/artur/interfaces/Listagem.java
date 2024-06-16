4cec60d40c6289f40949dfae9d05a0f7f204a226
Vectron7 <90154484+Vectron7@users.noreply.github.com>
2024-06-16 15:09:07 -0300

    Listagem.java

--- src/main/java/com/artur/interfaces/Listagem.java
+++ src/main/java/com/artur/interfaces/Listagem.java
@@ -1,3 +1,5 @@
+//Esse código define uma interface Listagem em Java com um método listar(), que deve ser implementado para listar itens.
+
 package com.artur.interfaces;
 
 public interface Listagem {
