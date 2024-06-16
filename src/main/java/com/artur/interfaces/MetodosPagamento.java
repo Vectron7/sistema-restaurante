cd29f7fa7af6933367a48dff3673434ce2c9a9e7
Vectron7 <90154484+Vectron7@users.noreply.github.com>
2024-06-16 15:10:50 -0300

    MetodosPagamento.java

--- src/main/java/com/artur/interfaces/MetodosPagamento.java
+++ src/main/java/com/artur/interfaces/MetodosPagamento.java
@@ -1,3 +1,5 @@
+//Este código Java define uma interface MetodosPagamento que declara métodos para diferentes formas de pagamento utilizando um GerenciadorMenu e um Scanner como parâmetros.
+
 package com.artur.interfaces;
 
 import com.artur.gerenciamento.GerenciadorMenu;
