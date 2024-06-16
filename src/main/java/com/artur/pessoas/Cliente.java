4fd084d092c0454bbe38c68b690e54a775c8e3f2
Vectron7 <90154484+Vectron7@users.noreply.github.com>
2024-06-16 15:13:02 -0300

    Cliente.java

--- src/main/java/com/artur/pessoas/Cliente.java
+++ src/main/java/com/artur/pessoas/Cliente.java
@@ -1,3 +1,5 @@
+//Este código Java define a classe Cliente, que é uma subclasse de Pessoa e implementa a interface Identificacao. Ela possui atributos privados para armazenar a data de nascimento (dataNasc) e o identificador único do cliente (idCliente). O construtor Cliente recebe o nome, endereço, telefone e data de nascimento do cliente e inicializa esses atributos utilizando o construtor da classe Pessoa (superclasse). O método getId() sobrescrito retorna o identificador único do cliente.
+
 package com.artur.pessoas;
 
 import com.artur.interfaces.Identificacao;
